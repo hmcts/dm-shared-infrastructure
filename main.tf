9b2973be2378423a466bacfa94a8c929d4f73a35
Darren Bromwich <darrenb@kainos.com>
2022-09-07 13:59:11 +0100

    updated sa_subnets to only add cft_aks_subnets if for prod

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ locals {
   )}"
 
   arm_aks_subnets = ["${data.azurerm_subnet.ase.id}", "${data.azurerm_subnet.aks-01.id}", "${data.azurerm_subnet.aks-00.id}"]
-  cft_aks_subnets = var.env == "prod" ? ["${data.azurerm_subnet.cft-aks-00.id}", "${data.azurerm_subnet.cft-aks-01.id}"] : []
+  cft_aks_subnets = var.env == "aat" ? [] : ["${data.azurerm_subnet.cft-aks-00.id}", "${data.azurerm_subnet.cft-aks-01.id}"]
 }
 
 resource "azurerm_resource_group" "shared_rg" {
