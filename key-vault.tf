0cd1d58ed40688e4374e36eb51396b2491b0a0b3
Hannah Smith <hannah.smith16@justice.gov.uk>
2023-06-30 10:21:12 +0100

    create new managed identity in correct subscription

--- key-vault.tf
+++ key-vault.tf
@@ -8,6 +8,7 @@ module "shared_vault" {
   resource_group_name        = azurerm_resource_group.shared_rg.name
   product_group_object_id    = "be8b3850-998a-4a66-8578-da268b8abd6b"
   managed_identity_object_id = var.managed_identity_object_id
+  create_managed_identity    = true
   common_tags                = local.tags
 }
 
