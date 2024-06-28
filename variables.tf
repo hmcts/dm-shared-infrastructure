ba37d4159c34368a1204054645b1060c9b9bacce
58060 <62423932+hmcts-jenkins-d-to-i[bot]@users.noreply.github.com>
2024-06-28 06:17:13 +0000

    Updating Terraform Formatting

--- variables.tf
+++ variables.tf
@@ -1,30 +1,30 @@
-variable product {
+variable "product" {
   default = "dm"
 }
 
-variable location {
+variable "location" {
   default = "UK South"
 }
 
-variable env {}
+variable "env" {}
 
-variable tenant_id {}
+variable "tenant_id" {}
 
-variable jenkins_AAD_objectId {}
+variable "jenkins_AAD_objectId" {}
 
 // TAG SPECIFIC VARIABLES
-variable common_tags {
+variable "common_tags" {
   type = map(string)
 }
 
-variable team_contact {
+variable "team_contact" {
   description = "The name of your Slack channel people can use to contact your team about your infrastructure"
   default     = "#ccd-devops"
 }
 
-variable destroy_me {
+variable "destroy_me" {
   description = "Here be dragons! In the future if this is set to Yes then automation will delete this resource on a schedule. Please set to No unless you know what you are doing"
   default     = "No"
 }
 
-variable aks_infra_subscription_id {}
+variable "aks_infra_subscription_id" {}
